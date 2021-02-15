ea0c47dfb58fc315e5fa652bbaa73d2b0b60d4ec
Mario Campopiano <mcampopiano92@gmail.com>
2021-02-15 10:44:40 -0600

    created image model

--- gamerraterapi/models/review.py
+++ gamerraterapi/models/review.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.db.models.fields import IntegerField
+from django.db.models.deletion import CASCADE
 from django.db.models.fields.related import ForeignKey
 
-class Rating(models.Model):
+class Review(models.Model):
     player = models.ForeignKey("Player", on_delete=models.CASCADE)
     game = models.ForeignKey("Game", on_delete=models.CASCADE)
-    rating = IntegerField()
+    content = models.CharField(max_length=250)
